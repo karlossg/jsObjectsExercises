5d4d38fff91c9693254d77ee61c9d2982c71fda8
Karol <karol.gratus@gmail.com>
2017-11-15 10:02:54 +0100

    selfthis finished

--- selfThis.js
+++ selfThis.js
@@ -1,9 +1,26 @@
-function Button() {
+// vanilla JS
+
+function Button(text) {
   this.text = text || 'Hello';
 }
 
 Button.prototype = {
-	create: function() {
-		this.element = getElementByTag= ('<button>');
+  create: function() {
+    const self = this;
+    this.element = document.createElement('button');
+    this.body = document.createTextNode(this.text);
+    this.element.appendChild(this.body);
+    this.element.onclick = () => {
+      alert(self.text);
+    }
+    document.body.appendChild(this.element);
 	}
 }
+
+const btn1 = new Button('Hi there!');
+const btn2 = new Button('My name is Mr. Button!');
+const btn3 = new Button();
+
+btn1.create();
+btn2.create();
+btn3.create();
